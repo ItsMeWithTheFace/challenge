22a906ec642452d77d5156bbb546608d181e64b9
rakinuddin <rakinuddin97@gmail.com>
2019-01-09 01:01:16 -0500

    updated schema, added knock auth

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,12 @@ ActiveRecord::Schema.define(version: 2019_01_08_170052) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "last_login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
