9c439116990d847e1328cdba394b222ebf1bb294
rakinuddin <rakinuddin97@gmail.com>
2019-01-09 00:58:36 -0500

    updated cart model, added user model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,4 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :product
-
-  validates_presence_of :fulfilled
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates_length_of       :password, minimum: 8, allow_nil: false, allow_blank: false
+  validates_confirmation_of :password, allow_nil: false, allow_blank: false
+
+  before_validation {
+    self.email = self.email.to_s.downcase
+  }
+
   has_many :cart
+
+  validates_presence_of     :email
+  validates_presence_of     :password
+  validates_uniqueness_of   :email
 end
